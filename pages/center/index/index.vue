43f136430447b44a9f97eb7de3bfd779fdb5a186
Liu_CD <uidq2003@v01.net>
2019-08-21 18:10:36 +0800

    ds

--- pages/center/index/index.vue
+++ pages/center/index/index.vue
@@ -1,143 +1,193 @@
 <template>
-	<view class="uni-container">
-		<!-- 已登录 -->
-		<view class="loginlogtitle" v-if="islogin">
-			<view class="headWarp">
-				<image class="headpic" src="/static/images/center/head@2x.png" />
-				<view class="headInfor">
-					<view class="num">
-						{{telNumSecret}}
-					</view>
-					<view class="tags">
-						<!-- 已认证 -->
-						<view class="tagWarp" v-if="iscertification">
-							{{certificationTag}}
+  <view class="uni-container">
+    <!-- 已登录 -->
+    <view class="loginlogtitle" v-if="islogin">
+      <view class="headWarp">
+        <image class="headpic" src="/static/images/center/head@2x.png" />
+        <view class="headInfor">
+          <view class="num">{{telNumSecret}}</view>
+          <view>
+            <!-- 已认证 -->
+            <view class="tagWarp" v-if="iscertification">{{certificationTag}}</view>
+            <!-- 未认证 -->
+            <view class="tagWarp" v-else>
+              <p>去认证</p>
+              <image class="path15" src="/static/images/center/path15@2x.png" />
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!-- 未登录 -->
+    <view v-else class="logtitle">
+      <image class="imgpic" src="/static/images/center/centerlogo@2x.png" />
+      <view class="loginBtn" @click="gotologin">立即登录</view>
+    </view>
 
-						</view>
-						<!-- 未认证 -->
-						<view class="tagWarp" v-else>
-
-
-						</view>
-
-					</view>
-
-				</view>
-
-			</view>
-
-		</view>
-		<!-- 未登录 -->
-		<view v-else class="logtitle">
-			<image class="imgpic" src="/static/images/center/centerlogo@2x.png" />
-			<view class="loginBtn">立即登录</view>
-		</view>
-
-		<view class="listWarp">
-			<uni-list>
-				<uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="消息通知" />
-				<uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="我的发布" />
-				<uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="草稿箱" />
-			</uni-list>
-			<uni-list>
-				<uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="我的收藏" />
-				<uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="设置" />
-			</uni-list>
-		</view>
-	</view>
+    <view class="listWarp">
+      <!-- 已认证 -->
+      <view v-if="iscertification">
+        <uni-list>
+          <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="消息通知" />
+          <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="我的发布" />
+          <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="草稿箱" />
+        </uni-list>
+        <uni-list class="item">
+          <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="我的收藏" />
+          <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="设置" />
+        </uni-list>
+      </view>
+      <!-- 未认证 -->
+      <uni-list v-else>
+        <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="消息通知" />
+        <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="我的发布" />
+        <uni-list-item :show-extra-icon="true" :extra-icon="extraIcon1" title="草稿箱" />
+      </uni-list>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		computed: {
-			// 格式化电话号码
-			telNumSecret() {
-				let reg = /^(\d{3})\d{4}(\d{4})$/;
-				return this.telNum.replace(reg, "$1****$2");
-			}
-		},
-		data() {
-			return {
-				extraIcon1: {
-					color: '#007aff',
-					size: '22',
-					type: 'settings'
-				},
-				islogin: true, //是否已登录
-				iscertification: false, //是否已认证
-				certificationTag: '村民认证', //认证标识
-				telNum: '13654425566'
-			}
-		}
-	}
+export default {
+  computed: {
+    // 格式化电话号码
+    telNumSecret() {
+      let reg = /^(\d{3})\d{4}(\d{4})$/;
+      return this.telNum.replace(reg, "$1****$2");
+    }
+  },
+  data() {
+    return {
+      extraIcon1: {
+        color: "#007aff",
+        size: "22",
+        type: "settings"
+      },
+      islogin: false, //是否已登录
+      iscertification: true, //是否已认证
+      certificationTag: "村民认证", //认证标识
+      telNum: "13654425566"
+    };
+  },
+  methods: {
+    gotologin() {
+		console.log("444")
+      uni.navigateTo({
+        url: "/pages/center/login/index"
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped="scoped">
-	.loginlogtitle {
-		height: 320upx;
-		background: linear-gradient(135deg, rgba(253, 99, 70, 1) 0%, rgba(197, 53, 53, 1) 100%);
+.loginlogtitle:before {
+  content: " ";
+  display: table;
+}
 
-		.headWarp {
-			height: 96upx;
-			padding-left: 30upx;
-			margin-top: 90upx;
+.loginlogtitle {
+  height: 320upx;
+  background: linear-gradient(
+    135deg,
+    rgba(253, 99, 70, 1) 0%,
+    rgba(197, 53, 53, 1) 100%
+  );
 
-			.headpic {
-				height: 100%;
-				width: 98upx;
-				display: inline-block;
+  .headWarp {
+    height: 96upx;
+    padding-left: 30upx;
+    margin-top: 112upx;
 
-			}
+    .headpic {
+      height: 100%;
+      width: 98upx;
+      float: left;
+    }
 
-			.headInfor {
-				height: 100%;
-				display: inline-block;
+    .headInfor {
+      height: 100%;
+      float: left;
+      margin-left: 32upx;
+      position: relative;
 
-				.num {}
+      .num {
+        font-size: 30upx;
+        font-family: PingFang-SC;
+        font-weight: bold;
+        color: rgba(255, 255, 255, 1);
+        line-height: 30upx;
+      }
 
-				.tags {
-					.tagWarp {}
-				}
-			}
-		}
-	}
+      .tagWarp {
+        width: 128upx;
+        height: 40upx;
+        border-radius: 4px;
+        border: 1px solid rgba(255, 217, 0, 1);
+        position: absolute;
+        bottom: 0;
+        text-align: center;
+        font-size: 22upx;
+        font-family: PingFangSC;
+        font-weight: 500;
+        color: rgba(255, 217, 0, 1);
+        p {
+          display: inline-block;
+          vertical-align: middle;
+        }
+        .path15 {
+          width: 14upx;
+          height: 24upx;
+          margin-left: 10upx;
+          vertical-align: middle;
+        }
+      }
+    }
+  }
+}
 
-	.logtitle {
-		height: 408upx;
-		background: linear-gradient(135deg, rgba(253, 99, 70, 1) 0%, rgba(197, 53, 53, 1) 100%);
-		text-align: center;
+.logtitle {
+  height: 408upx;
+  background: linear-gradient(
+    135deg,
+    rgba(253, 99, 70, 1) 0%,
+    rgba(197, 53, 53, 1) 100%
+  );
+  text-align: center;
 
+  .imgpic {
+    width: 448upx;
+    height: 114upx;
+    margin: 80upx 0 60upx;
+  }
 
+  .loginBtn {
+    margin: auto;
+    width: 340upx;
+    height: 88upx;
+    text-align: center;
+    box-shadow: 0px 0px 26px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 48upx;
+    border: 1px solid rgba(255, 255, 255, 1);
+    font-size: 36upx;
+    font-family: PingFangSC;
+    font-weight: 500;
+    color: rgba(255, 255, 255, 1);
+    line-height: 88upx;
+  }
+}
 
-		.imgpic {
-			width: 448upx;
-			height: 114upx;
-			margin: 80upx 0 60upx;
-		}
+.listWarp {
+  margin-top: 20upx;
+  .item {
+    margin-top: 20upx;
+  }
 
-		.loginBtn {
-			margin: auto;
-			width: 340upx;
-			height: 88upx;
-			text-align: center;
-			box-shadow: 0px 0px 26px 0px rgba(0, 0, 0, 0.1);
-			border-radius: 48upx;
-			border: 1px solid rgba(255, 255, 255, 1);
-			font-size: 36upx;
-			font-family: PingFangSC;
-			font-weight: 500;
-			color: rgba(255, 255, 255, 1);
-			line-height: 88upx;
-
-		}
-	}
-
-	.listWarp {
-		margin-top: 20upx;
-
-		.uni-list-item__content-title {
-			font-size: 30upx;
-
-		}
-	}
+  /deep/ .uni-list-item__content-title {
+    font-size: 30upx;
+    font-family: PingFang-SC;
+    font-weight: 500;
+    color: rgba(51, 51, 51, 1);
+  }
+}
 </style>
---
+++ pages/center/login/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <view class="uni-container">
+    <view>
+      <image class="loginpic" src="/static/images/center/loginPic@2x.png" />
+    </view>
+
+    <view class="coreWarp">
+      <view>
+        <input class="uni-input" type="number" placeholder="请输入手机号码" />
+      </view>
+      <view class="btn">获取验证码</view>
+      <view class="afreshGet">
+        没收到？
+        <span>重新获取</span>
+      </view>
+    </view>
+    <view class="otherLogin">
+      <span class="line">使用社交账号登录</span>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  computed: {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped="scoped">
+.uni-container {
+  text-align: center;
+  height: 100%;
+  background: #fff;
+  position: relative;
+}
+.loginpic {
+  width: 144upx;
+  height: 200upx;
+  margin: 82upx 0 56upx;
+}
+.coreWarp {
+  .uni-input {
+    width: 622upx;
+    height: 108upx;
+    margin: 0 auto 64upx;
+    border-bottom: 1upx solid #eee;
+    padding: 0;
+  }
+  .btn {
+    width: 630upx;
+    height: 88upx;
+    margin: 0 auto;
+    line-height: 88upx;
+    background: rgba(204, 204, 204, 1);
+    border-radius: 200upx;
+    font-size: 32upx;
+    font-family: PingFangSC;
+    font-weight: 500;
+    color: rgba(255, 255, 255, 1);
+  }
+  .afreshGet {
+    text-align: left;
+    padding-top: 24upx;
+    padding-left: 104upx;
+    font-size: 24upx;
+    font-family: PingFangSC;
+    font-weight: 400;
+    color: rgba(153, 153, 153, 1);
+    span {
+      color: rgba(197, 53, 53, 1);
+    }
+  }
+}
+.otherLogin {
+    position: fixed;
+    bottom: 0;
+    height: 356upx;
+    width: 100%;
+    
+    .line {
+      position: relative;
+      font-size: 22upx;
+      color: #999999;
+      text-align: center;
+
+      &:before {
+        content: "";
+        display: block;
+        width: 122upx;
+        height: 1upx;
+        background: #eaeaea;
+        position: absolute;
+        right: 200upx;
+        top: 14upx;
+      }
+
+      &:after {
+        content: "";
+        display: block;
+        width: 122upx;
+        height: 1upx;
+        background: #eaeaea;
+        position: absolute;
+        left: 200upx;
+        top: 14upx;
+      }
+    }
+  }
+</style>
